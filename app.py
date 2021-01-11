f871b4e1dd92bbb5cbd826aa90697723f905872f
SuiPing <kam.suipingooi@me.com>
2021-01-11 10:40:14 +0000

    flasklabs

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import random
 
 app = Flask(__name__)
 
 # your code here
 
+
 @app.route('/')
 def home():
-    return "hello world"
+    return render_template('home.template.html')
+
 
 @app.route('/about-us')
 def about_us():
-    return "About Us"
+    return "<h1>About Us</h1>"
+
+@app.route('/our-history')
+def our_history():
+    return render_template('history.template.html')
+
+@app.route('/lucky')
+def lucky_number():
+    number = random.randint(1000, 9999)
+    return "<h1>Your lucky number is </h1>" + str(number)
+
 
 # "magic code" -- boilerplate
-
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP')),
-    port = int(os.environ.get('PORT')),
-    debug = True
+    app.run(host='localhost',
+            port=8080,
+            debug=True)
